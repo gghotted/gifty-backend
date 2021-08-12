e4435926e1a58d6f93145ef1eadb82c660d9665d
gghotted <gghotted2@naver.com>
2021-08-12 13:42:56 +0900

    Update admin

--- gifty/admin.py
+++ gifty/admin.py
@@ -1,62 +1,67 @@
 from django.contrib import admin
-from django.db import models
-from django.urls import path
-from django.template.response import TemplateResponse
 from django.contrib.auth.models import Group
-from .models import User
+from .models import *
+
+
+class GenderCategoryInline(admin.TabularInline):
+    model = GenderCategory
+    fields = ('is_active',)
+    extra = 0
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class PriceCategoryInline(admin.TabularInline):
+    model = PriceCategory
+    fields = ('is_active',)
+    extra = 0
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class AgeCategoryInline(admin.TabularInline):
+    model = AgeCategory
+    fields = ('is_active',)
+    extra = 0
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(AppManager)
+class AppManagerAdmin(admin.ModelAdmin):
+    inlines = (GenderCategoryInline, PriceCategoryInline, AgeCategoryInline)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'category', 'description', 'vendor', 'views', 'likes')
+    list_display_links = list_display
+    list_filter = ('gender', 'age', 'price')
+
 
 
 admin.site.unregister(Group)
 admin.site.register(User)
-
-# https://stackoverflow.com/questions/10053981/how-can-i-create-custom-page-for-django-admin
-class AppManager(models.Model):
-    class Meta:
-        app_label = 'gifty'
-        verbose_name = '앱관리'
-        verbose_name_plural = verbose_name
-
-
-class CustomModelAdmin(admin.ModelAdmin):
-    template_name = ''
-    url = ''
-
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path(self.url, self.admin_site.admin_view(self.view))
-        ]
-        return custom_urls + urls
-
-    def view(self, request):
-        if request.method == 'GET':
-            return self.get(request)
-        if request.method == 'POST':
-            return self.post(request)
-
-    def get(self, request):
-        return self.render_to_response(request)
-
-    def post(self, request):
-        pass
-
-    def get_context_data(self, request):
-        return dict(self.admin_site.each_context(request))
-
-    def render_to_response(self, request):
-        return TemplateResponse(request, self.template_name, self.get_context_data(request))
-
-
-@admin.register(AppManager)
-class AppManagerAdmin(CustomModelAdmin):
-    template_name = 'gifty/app_manage.html'
-    url = ''
-
-    def get_context_data(self, request):
-        context = super().get_context_data(request)
-
-        return context
-
-
-
-
+admin.site.register(ProductCategory)
+# admin.site.register(AppManager)
+# admin.site.register(AgeCategory)
+# admin.site.register(PriceCategory)
+# admin.site.register(GenderCategory)
--- gifty/models.py
+++ gifty/models.py
@@ -10,22 +10,54 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class AgeCategory(BaseModel):
-    min = models.PositiveIntegerField(null=True)
-    max = models.PositiveIntegerField(null=True)
-    str = models.CharField(max_length=32, null=True)
-    is_active = models.BooleanField(default=True)
+class AppManager(BaseModel):
+    class Meta:
+        verbose_name = ' 앱관리'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
-        return self.str or f'{self.min}~{self.max}'
+        return self._meta.verbose_name
+
+
+class GenderCategory(BaseModel):
+    value = models.CharField(max_length=16)
+    is_active = models.BooleanField(default=True, verbose_name='활성화')
+    manager = models.ForeignKey(AppManager, on_delete=models.DO_NOTHING, default=1, related_name='genders')
+
+    class Meta:
+        verbose_name = '성별 카테고리'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.value
+
+
+class AgeCategory(BaseModel):
+    min = models.PositiveIntegerField(null=True, blank=True)
+    max = models.PositiveIntegerField(null=True, blank=True)
+    str = models.CharField(max_length=32, blank=True)
+    is_active = models.BooleanField(default=True, verbose_name='활성화')
+    manager = models.ForeignKey(AppManager, on_delete=models.DO_NOTHING, default=1, related_name='ages')
+
+    class Meta:
+        verbose_name = '나이 카테고리'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.str or f'{self.min}~{self.max}세'
 
 
 class PriceCategory(BaseModel):
     value = models.PositiveIntegerField()
-    str = models.CharField(max_length=32, null=True)
-    is_active = models.BooleanField(default=True)
+    str = models.CharField(max_length=32, blank=True)
+    is_active = models.BooleanField(default=True, verbose_name='활성화')
+    manager = models.ForeignKey(AppManager, on_delete=models.DO_NOTHING, default=1, related_name='prices')
 
-    def __str__(self) -> str:
+    class Meta:
+        verbose_name = '가격 카테고리'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
         return self.str or f'{self.value}원'
 
 
@@ -34,7 +66,40 @@ class User(AbstractUser):
         verbose_name_plural = '회원관리'
 
 
+class ProductCategory(BaseModel):
+    value = models.CharField(max_length=32)
+
+    class Meta:
+        verbose_name = '상품 카테고리'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.value
+
+
 class Product(BaseModel):
-    pass
+    code = models.AutoField(primary_key=True, verbose_name='코드')
+    name = models.CharField(max_length=128, verbose_name='상품명')
+    category = models.ForeignKey(ProductCategory, verbose_name='상품유형', related_name='products', on_delete=models.CASCADE)
+    # img = models.ImageField(verbose_name='썸네일')
+    description = models.TextField(blank=True, verbose_name='상품소개')
+    vendor = models.CharField(max_length=64, verbose_name='판매처')
+    views = models.PositiveIntegerField(default=0, verbose_name='노출수')
+    likes = models.PositiveIntegerField(default=0, verbose_name='좋아요수')
+    # likes = models.PositiveIntegerField(default=0, verbose_name='주문수')
+    gender = models.ManyToManyField(GenderCategory, verbose_name='성별', related_name='products')
+    age = models.ManyToManyField(AgeCategory, verbose_name='연령', related_name='products')
+    price = models.ForeignKey(PriceCategory, verbose_name='가격', related_name='products', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '상품'
+        verbose_name_plural = '상품관리'
+
+    def __str__(self):
+        return self.name
+
+
+
+
 
 
